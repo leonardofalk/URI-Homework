ed3f6ed5aca9bf97310b93af1cb8e3d2dd6a2f4f
Leonardo Falk <leonardo.falk@hotmail.com>
2014-10-10 08:48:33 -0300

    Update 1249 - Rot13.cpp

--- 1250 - Strings Facil/1249 - Rot13.cpp
+++ 1250 - Strings Facil/1249 - Rot13.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
- 
+
 using namespace std;
+
 int main() {
     string linha;
     char c;
+    
     while (getline(cin,linha)) {
-        for (int i=0; i<linha.size(); i++) {
+    	int tam = linha.size();
+    	
+        for (int i = 0; i < tam; i++) {
+        	/// isalpha retorna true se for uma letra do alfabeto, se nao então falso
             if(isalpha(linha[i])) {
+            	/// isupper retorna true se a letra é maiuscula, se não então falso
                 if(isupper(linha[i])) {
-                    c = ((linha[i]+13)%91);
+                	/// se a linha atual mais 13 ultrapassar 90 ('Z') então fazemos o modulo por 91
+                	/// retornando ao estado a partir do inicio da lista de caracteres
+                    c = ((linha[i] + 13) % 91);
+                    
                     if (c < 40) {
-                        c=c+'A';
+                        c = c + 'A';
                     }
                 } else {
+                	/// se a linha atual mais 13 ultrapassar 123 ('z') então fazemos o modulo por 123
+                	/// retornando ao estado a partir do inicio da lista de caracteres
                     c = ((linha[i]+13)%123);
+                    
                     if (c < 40) {
-                        c=c+'a';
+                        c = c + 'a';
                     }
                 }
-                cout <<c;
+                
+                cout << c;
             } else {
                 cout << linha[i];
             }
         }
-        cout <<endl;
+        
+        cout << endl;
     }
+    
     return 0;
 }
