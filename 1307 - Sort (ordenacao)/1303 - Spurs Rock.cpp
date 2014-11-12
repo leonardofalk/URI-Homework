b4f32e8d378847a0f26a9831867c1df55595b3f8
Leonardo Falk <leonardo.falk@hotmail.com>
2014-11-12 21:34:10 -0200

    Update 1303 - Spurs Rock.cpp

--- 1307 - Sort (ordenacao)/1303 - Spurs Rock.cpp
+++ 1307 - Sort (ordenacao)/1303 - Spurs Rock.cpp
@@ -1,85 +1,71 @@
-/*
-	20% wrong answer
-*/
+/* by elosilva */
 
-#include <iostream>
-#include <algorithm>
- 
+#include<iostream> // fixed typo here
+#include<algorithm>
+#include<cstdio>
+#include<iomanip>
+
 using namespace std;
- 
-struct Team {
-    int id = 0, won = 0, points = 0;
-    double avg = 0;
+
+struct camp {
+int id, p, g, e; /// p= pontos; g=pontos feitos (goals); e=erros=pontos contra;
+double ca;  // (cesta average)
 };
- 
-bool comp(Team a, Team b) {
-    if (a.won == b.won) {
-        if (a.avg == b.avg) {
-            if (a.points == b.points) {
-                return a.id < b.id;
-            }
-             
-            return a.points > b.points;
+
+bool comp ( camp a ,camp b ){
+    if ( a.p == b.p ){             // pontos =
+        if ( a.ca != b.ca ){       //cesta average !=
+            return a.ca > b.ca;
         }
-         
-        return a.avg > b.avg;
-    }
-     
-    return a.won > b.won;
+        else {                    //cesta average = 
+            if ( a.p != b.p ){    // pontos marcados !=
+                return a.g > b.g; 
+            }
+            else{                 //pontos marcados =
+                return a.id > b.id;
+            }
+        }
+        }
+    return a.p > b.p;
 }
- 
-int main() {
-    int cases, id1, points1, id2, points2, instancia = 1;
-     
-    while (cin >> cases) {
-        if (cases == 0) {
-            return 0;
+
+int main(){
+    cout<<fixed<<setprecision(4);  //frescura da minha parte
+    //freopen("1303.in","r",stdin);
+    int n, m, a, b, x, y, cont=0;
+
+    while ( cin>>n ){
+        if ( n==0 ){break;}             // se n =0 então sai fora do while;
+        camp time[n+1];
+        cont++;                         //contador instâncias;
+        if (cont>1){cout<<"\n";}        //se tiver 2 ou + instâncias deve-se separar os casos de teste com uma linha em branco
+        cout<<"Instancia "<<cont<<endl;
+        for ( int i=0 ; i<=n ; i++ ){    //zera a pontuação dos times para iniciar um novo teste e usar ex.: x+=y ou x++;
+            time[i].id=i;                // definição do id;
+            time[i].p=time[i].g=time[i].e=time[i].ca=0;
         }
-         
-        int tam = (cases * (cases - 1)) / 2;
-         
-        Team vet[cases];
-         
-        for (int i = 1; i <= tam; i++) {
-            cin >> id1 >> points1 >> id2 >> points2;
-             
-            vet[id1 - 1].id = id1;
-            vet[id2 - 1].id = id2;
-            vet[id1 - 1].points += points1;
-            vet[id2 - 1].points += points2;
-             
-            if (points1 == 0) {
-                vet[id2 - 1].avg += points2;
-            } else if (points2 != 0) {
-                vet[id2 - 1].avg += points2 / points1;
+        m=n*(n-1)/2;                    // calculo do numero de jogos
+        for ( int i=0 ; i<m ; i++ ){   //entrada dos placares
+            cin>>a>>x>>b>>y;
+            time[a].g+=x;time[a].e+=y;   //atribuição de pontos
+            time[b].g+=y;time[b].e+=x;
+            if (x<y){time[a].p+=1;time[b].p+=2;}
+            if (x>y){time[a].p+=2;time[b].p+=1;}
+        }
+        for ( int i=1 ; i<=n ; i++ ){   // calculo do CA
+            if ( time[i].e==0 ){
+                time[i].ca=time[i].p;
             }
-             
-            if (points2 == 0) {
-                vet[id1 - 1].avg += points1;
-            } else if (points1 != 0) {
-                vet[id1 - 1].avg += points1 / points2;
-            }
-             
-             
-            if (points1 > points2) {
-                vet[id1 - 1].won += 2;
-                vet[id2 - 1].won++;
-            } else if (points1 < points2) {
-                vet[id2 - 1].won += 2;
-                vet[id1 - 1].won++;
-            } else {
-                 
+            else {  // multipliquei por 0.0...1 para trabalhar com double, quando eu usava int não conseguia o resultado esperado;
+                time[i].ca=(time[i].g*1.00000001)/(time[i].e*1.00000001); //ca tem que ser em double!
             }
         }
-         
-        sort(vet, vet + cases, comp);
-         
-        cout << ((instancia > 1) ? "\n" : "") << "Instancia " << instancia++ << "\n";
-         
-        for (int j = 0; j < cases; j++) {
-            cout << ((j > 0) ? " " : "") << vet[j].id;
+        sort( time+1, time+n+1, comp ); //sort
+        for ( int i=1 ; i<n ; i++ ){   //cout do problema
+            cout<<time[i].id<<" ";
         }
-     
-        cout << "\n";
+        cout<<time[n].id;
+        cout<<"\n";
     }
+    return 0;
 }
