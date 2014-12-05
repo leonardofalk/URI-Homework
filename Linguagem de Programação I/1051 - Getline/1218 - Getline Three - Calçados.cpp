aa7dc47220afcb2ce3004254937ab8f799cd710b
Leonardo Falk <leonardo.falk@hotmail.com>
2014-12-05 09:01:23 -0200

    Update 1218 - Getline Three - Calçados.cpp

--- Linguagem de Programação I/1051 - Getline/1218 - Getline Three - Calçados.cpp
+++ Linguagem de Programação I/1051 - Getline/1218 - Getline Three - Calçados.cpp
@@ -1,6 +1,6 @@
-#include iostream
-#include string
-#include sstream
+#include <iostream>
+#include <string>
+#include <sstream>
  
 using namespace std;
  
@@ -10,13 +10,28 @@ int main() {
  
     while (cin  nx) {
         int m = 0, f = 0, same = 0, n;
-        cin.get();
+		cin.get();
  
         getline(cin, todos);
  
         stringstream sapatos(todos);
- 
-        while (sapatos  n  sexo) {
+        /*
+        	todos = "01 M 02 F 03 M ...";
+        	
+        	depois do 'stringstream'
+        	
+        	sapatos >> str = "01";
+        	sapatos >> str = "M";
+        	sapatos >> str = "02";
+        	sapatos >> str = "F";
+        	sapatos >> str = "03";
+        	sapatos >> str = "M";
+        	...
+        	
+        	usamos nesse caso 2 variaveis: uma para o numero do calçado e outra para o genero
+        	o stringstream vai pegar 2 valores em cada rodada e colocar nessas variaveis
+        */
+        while (sapatos >> n >> sexo) {
             if (n == nx) {
                 same++;
                  
@@ -28,14 +43,14 @@ int main() {
             }
         }
  
-        if (caso  1) {
-            cout  endl;
+        if (caso > 1) {
+            cout << endl;
         }
  
-        cout  Caso   caso  n;
-        cout  Pares Iguais   same  endl;
-        cout  F   f  endl;
-        cout  M   m  endl;
+        cout << "Caso: " << caso << endl;
+        cout << "Pares Iguais: "<< same << endl;
+        cout << "F: " << f << endl;
+        cout << "M: " << m << endl;
         caso++;
     }
 }
