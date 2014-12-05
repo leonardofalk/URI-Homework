dcabd1509baf7733e1000df8ceb0c5f352a6569e
Leonardo Falk <leonardo.falk@hotmail.com>
2014-12-05 08:55:07 -0200

    Update 1217 - Getline Two - Frutas.cpp

--- Linguagem de Programação I/1051 - Getline/1217 - Getline Two - Frutas.cpp
+++ Linguagem de Programação I/1051 - Getline/1217 - Getline Two - Frutas.cpp
@@ -17,11 +17,37 @@ int main() {
         totalp += price;
         getline(cin, fruits);
  
-        stringstream amount(fruits);
+        stringstream DIVIDE_STRING(fruits);
+        /*
+			eplicação de como funciona 'stringstream'
+			string base: "banana melancia uva tomate"
+			
+			stringstream split("banana melancia uva tomate")
+			
+			Vai simular um loop com 4 saidas, como se fossem 4 valores lidos com 'cin' linha por linha
+			
+			1ª volta do laço
+			split >> str; => "banana"
+			2ª volta do laço
+			split >> str; => "melancia"
+			3ª volta do laço
+			split >> str; => "uva"
+			4ª volta do laço
+			split >> str; => "tomate"
+			
+			A variavel pode ser a mesma pois a cada volta ele sobrescreve o valor na mesma.
+        */
+        
         int f = 0;
-        while (amount >> fruit) {
+        
+        while (DIVIDE_STRING >> fruit) {
+        	/*
+        		a cada fruta lida soma um valor no contador
+        		o nome delas nao é importante nesse caso e sim a quantidade
+        	*/
             f++;
         }
+        
         totalf += f;
         cout << "day " << i << ": " << f << " kg\n";
     }
