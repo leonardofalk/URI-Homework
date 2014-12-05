5f0a8c37ece165e2b233fcba1423ec6018490a50
Leonardo Falk <leonardo.falk@hotmail.com>
2014-12-05 09:38:28 -0200

    Update 1430 - Composição de Jingles.cpp

--- Linguagem de Programação I/1401 - Mapas (STL)/1430 - Composição de Jingles.cpp
+++ Linguagem de Programação I/1401 - Mapas (STL)/1430 - Composição de Jingles.cpp
@@ -6,6 +6,14 @@ using namespace std;
 
 int main() {
 	map <char, double> duration = {
+		/*
+			é possivel definir o mapa com os valores prontos dessa forma
+			desde que não esteja sendo compilado no codeblocks, recomendo
+			usar o compilador online: http://ideone.com
+			
+			{CHAVE, VALOR},
+		*/
+		
 		{'W', 1.0},
 		{'H', 0.5},
 		{'Q', 0.25},
@@ -23,13 +31,25 @@ int main() {
 		
 		for (int i = 0; i < tam; i++) {
 			if (notes[i] == '/') {
+				/*
+					se o caractere lido é a barra então terminou uma seção de valores
+					temos que verificar se a seção terminou com 1 segundo pois é o tempo
+					exato de cada seção
+				*/
 				if (total == 1) {
 					count++;
 				}
 				
+				/*
+				  	reseta o contador de tempo toda vez mesmo estando com valor diferente de 1 segundo
+				*/
 				total = 0;
 			} else {
-				total += duration[notes[i]];	
+				/*
+					se for diferente de '/' entao é a chave de um dos valores
+					só precisa ser somada com o valor correspondente no mapa
+				*/
+				total += duration[ notes[i] ];	
 			}
 		}
 		
