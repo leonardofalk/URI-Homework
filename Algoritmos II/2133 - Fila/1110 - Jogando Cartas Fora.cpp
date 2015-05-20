4398583871f072528aa97d47858eb5cfbaf78ab3
Julio Cesar Ceron <julio.ceron@live.com>
2015-05-19 21:30:09 -0300

    Create 1110 - Jogando Cartas Fora.cpp

---
+++ Algoritmos II/2133 - Fila/1110 - Jogando Cartas Fora.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+ 
+ 
+using namespace std;
+ 
+ 
+int main(){
+    queue <int> q;
+    int n;
+    while((cin >> n) and n!=0){
+        for(int i=1; i<=n; i++){
+            q.push(i);
+        }
+        cout << "Discarded cards: ";
+        while (q.size()>2){
+            cout << q.front() << ", ";
+            q.pop();
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front();
+        q.pop();
+        cout << endl << "Remaining card: " << q.front() << endl;
+        while(!q.empty()){
+            q.pop();
+        }
+    }
+ 
+ 
+ return 0;
+}
